89892b03a2aa9f56a13b11cdbb591a49cc1f0dab
Darren Jackson <darren2150@hotmail.com>
2018-09-08 22:20:10 +0100

    add files

---
+++ MovingCircle/sketch.js
@@ -0,0 +1,15 @@
+var x = 0;
+
+function setup() {
+    createCanvas(320, 240);
+}
+
+function draw() {
+    background(0);
+    ellipse(x, 120, 16, 16);
+
+    x = x + 1;
+    if (x > width) {
+        x = 0;
+    }
+}
